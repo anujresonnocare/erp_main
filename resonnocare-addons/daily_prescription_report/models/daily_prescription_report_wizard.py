695b1d0f7c6b6330f15f9519650fe08075f952e2
Anuj Chauhan <anujchauhansss.ac@gmail.com>
2026-09-11 12:47:48 +0530

    added

--- resonnocare-addons/daily_prescription_report/models/daily_prescription_report_wizard.py
+++ resonnocare-addons/daily_prescription_report/models/daily_prescription_report_wizard.py
@@ -81,7 +81,7 @@ class DailyPrescriptionReportWizard(models.TransientModel):
             ('appointment_date', '>=', self.date_from),
             ('appointment_date', '<=', self.date_to),
             ('status', 'not in', ['cancelled', 'no_show']),
-            ('appointment_type_id', 'in', [467])
+            ('appointment_type_id', 'in', device_appointment_types.ids)
         ]
         
         if self.area_manager_id:
